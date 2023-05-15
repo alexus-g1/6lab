18b0c126ef007f1373f172926c2ce46f5532142f
alexus_g1 <antidot2002@yandex.ru>
2023-05-15 16:07:12 +0300

    1st commit in main

--- file.cpp
+++ file.cpp
@@ -3,7 +3,7 @@
 #include <math.h>
 void main()
 {
-	//1st comment
+	//1st commit in main
 	int a[20] = { 1, 2, 4, 5, 7, 5, 8, 4, 3, 2, 9, 5, 4, 6, 2, 7, 4, 5, 1, 3 };
 	int i, s1, s2;
 	for (i = 0, s1 = 0, s2 = 0; i < 20; ++i)
